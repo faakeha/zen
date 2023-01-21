652ce14a94236f768c457e1934cd5f5713b5e681
Faakeha Ahmed <faakehaahmed123@gmail.com>
2023-01-21 16:58:52 +0500

    firebase setup

---
+++ lib/screens/assessment/assessment.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Assessment extends StatefulWidget {
+  const Assessment({Key? key}) : super(key: key);
+
+  @override
+  State<Assessment> createState() => _AssessmentState();
+}
+
+class _AssessmentState extends State<Assessment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('asjfkldn'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const PrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF7DCFB6),Color(0XFFFBD1A2)],
+);
+
+const primaryColorPeach = Color(0xFFF79256);
+const primaryColorPeachLight = Color(0xFFFBD1A2);
+const primaryColorBlueLight = Color(0xFF7DCFB6);
+const primaryColorBlue = Color(0xFF00B2CA);
+const primaryColorBlueDark = Color(0xFF1D4E89);
