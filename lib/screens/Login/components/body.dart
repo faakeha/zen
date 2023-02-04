b2f43bfa5d64b901270d66cf256c369cbaa1e3c1
sukainasb <sukainasbb@gmail.com>
2023-02-04 13:55:46 +0500

    sukaina

---
+++ lib/screens/Login/components/body.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:zen/components/AlreadyHaveAnAccount.dart';
+import 'package:zen/components/RoundedButton.dart';
+import 'package:zen/components/TextFieldWidget.dart';
+//import 'package:zen/components/RoundedInputField.dart';
+//import 'package:zen/components/RoundedPasswordField.dart';
+import 'package:zen/screens/Login/components/background.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:zen/screens/constants.dart';
+import 'package:zen/screens/signup/signu_up_screen.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            RoundedTextField(hintText: "Your Email", onChanged: (value) {}),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(text: "LOGIN", press: () {}),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            AlreadyHaveAnAccount(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            )
+          ]),
+    );
+  }
+}
+
+class RoundedTextField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedTextField({
+    Key? key,
+    required this.hintText,
+    this.icon = Icons.person,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldWidget(
+        child: TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none),
+    ));
+  }
+}
+
+class TextFieldWidget extends StatelessWidget {
+  final Widget child;
+
+  const TextFieldWidget({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kPrimaryLightColor,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: child,
+    );
+  }
+}
+
+class RoundedPasswordField extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+  const RoundedPasswordField({
+    Key? key,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldWidget(
+        child: TextField(
+      obscureText: true,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: "Password",
+        icon: const Icon(
+          Icons.lock,
+          color: kPrimaryColor,
+        ),
+        suffixIcon: Icon(
+          Icons.visibility,
+          color: kPrimaryColor,
+        ),
+        border: InputBorder.none,
+      ),
+    ));
+  }
+}
---
+++ lib/screens/signup/components/body.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:zen/components/AlreadyHaveAnAccount.dart';
+import 'package:zen/components/RoundedButton.dart';
+import 'package:zen/components/RoundedPasswordField.dart';
+import 'package:zen/components/RoundedTextField.dart';
+import 'package:zen/screens/Login/login_screen.dart';
+import 'package:zen/screens/signup/components/background.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:zen/components/AlreadyHaveAnAccount.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "SIGN UP",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          SvgPicture.asset(
+            "assets/icons/signup.svg",
+            height: size.height * 0.35,
+          ),
+          RoundedTextField(hintText: "Your Email", onChanged: (value) {}),
+          RoundedPasswordField(onChanged: (value) {}),
+          RoundedButton(text: "SIGN UP", press: () {}),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          AlreadyHaveAnAccount(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+          OrDivider(),
+        ],
+      ),
+    );
+  }
+}
+
+class OrDivider {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.8,
+      child: Row(children: <Widget>[
+        Expanded(
+          child: Divider(),
+        )
+      ]),
+    );
+  }
+}
